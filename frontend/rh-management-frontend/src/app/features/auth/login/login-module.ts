50b17c2748204a1e31a6b8180c9a846cae9881a5
Kani Amine <kaniamine55@gmail.com>
2026-03-27 19:26:11 +0100

    update login

--- frontend/rh-management-frontend/src/app/features/auth/login/login-module.ts
+++ frontend/rh-management-frontend/src/app/features/auth/login/login-module.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class LoginModule {}
+export class LoginModule {
+  constructor(private router: Router) {}
+
+  onLogin(): void {
+    this.router.navigate(['/home-employee']);
+  }
+}
--- frontend/rh-management-frontend/src/app/features/auth/login/login.ts
+++ frontend/rh-management-frontend/src/app/features/auth/login/login.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+  constructor(private router: Router) {}
+
+  onLogin(): void {
+    this.router.navigate(['/home-employee']);
+  }
+}
